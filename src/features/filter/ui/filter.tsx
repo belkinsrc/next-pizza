492808dacce05e97291cd7190b66d60e6466a45d
Nikita Belkin <nikitabelkin233@gmail.com>
2024-11-24 14:06:27 +0300

    add Filter component

---
+++ src/features/filter/ui/filter.tsx
@@ -0,0 +1,18 @@
+import { FilterCheckbox } from "@components/shared/ui"
+import { cn } from "@components/shared/utils/utils"
+
+interface FilterProps {
+  className?: string
+}
+
+const Filter: React.FC<FilterProps> = ({ className }) => {
+  return (
+    <aside className={cn(className)}>
+      <h3>Фильтрация</h3>
+      <FilterCheckbox text="Популярные" value="popular" name="popular"/>
+    </aside>
+  )
+}
+
+export { Filter }
+
