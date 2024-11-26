1f7193988eea7983b1a16baf891117e7dfccbda5
Nikita Belkin <belkinsrc@gmail.com>
2024-11-26 18:42:10 +0300

    restructure Filter component

--- src/features/filter/ui/filter.tsx
+++ src/features/filter/ui/filter.tsx
@@ -1,5 +1,8 @@
-import { FilterCheckbox } from "@components/shared/ui"
-import { cn } from "@components/shared/utils/utils"
+import { cn } from "@components/shared/utils"
+import { FilterCheckbox, Title } from "@components/shared/ui"
+import { PriceFilter } from "./price-filter"
+import { IngredientsFilter } from "./ingredients-filter"
+import { DoughTypeFilter } from "./dough-type-filter"
 
 interface FilterProps {
   className?: string
@@ -7,9 +10,17 @@ interface FilterProps {
 
 const Filter: React.FC<FilterProps> = ({ className }) => {
   return (
-    <aside className={cn(className)}>
-      <h3>Фильтрация</h3>
-      <FilterCheckbox text="Популярные" value="popular" name="popular"/>
+    <aside className={cn(className, 'basis-[244px] flex-grow-0')}>
+      <Title text="Фильтрация" size="lg" className="mb-[25px] text-[22px]" />
+
+      <div className="flex flex-col gap-4 pb-[25px] mb-[25px] border-b-[1px] border-[#F6F6F6]">
+        <FilterCheckbox text="Можно собирать" value="configurable" name="configurable" />
+        <FilterCheckbox text="Новинки" value="new" name="new" />
+      </div>
+
+      <PriceFilter />
+      <IngredientsFilter />
+      <DoughTypeFilter />
     </aside>
   )
 }
