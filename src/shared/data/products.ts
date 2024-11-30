0574375473296104ccd419904ffe9332d7d1005e
Nikita Belkin <belkinsrc@gmail.com>
2024-11-30 18:59:03 +0300

    add mock data

---
+++ src/shared/data/products.ts
@@ -0,0 +1,22 @@
+const products = [
+  {
+    id: '1',
+    name: 'Пицца с мясом',
+    price: 10,
+    imageSrc: 'https://media.dodostatic.net/image/r:584x584/11EE7D612FC7B7FCA5BE822752BEE1E5.avif',
+  },
+  {
+    id: '2',
+    name: 'Пицца с мясом',
+    price: 10,
+    imageSrc: 'https://media.dodostatic.net/image/r:584x584/11EE7D612FC7B7FCA5BE822752BEE1E5.avif',
+  },
+  {
+    id: '3',
+    name: 'Пицца с мясом',
+    price: 10,
+    imageSrc: 'https://media.dodostatic.net/image/r:584x584/11EE7D612FC7B7FCA5BE822752BEE1E5.avif',
+  },
+]
+
+export { products }
