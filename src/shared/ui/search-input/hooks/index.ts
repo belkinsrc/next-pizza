a9e596dd07d796a1783a3681a30e92b01e142412
Nikita Belkin <belkinsrc@gmail.com>
2025-01-10 18:45:44 +0300

    delete extra component

--- src/shared/ui/search-input/hooks/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './useFetchSearchData'
-export * from './useDisableScroll'
--- src/shared/ui/search-input/utils/index.tsx
+++
@@ -1,6 +0,0 @@
-export const highlightText = (text: string, query: string) => {
-  const regex = new RegExp(`(${query})`, 'gi')
-  return text
-    .split(regex)
-    .map((part, index) => (regex.test(part) ? <b key={index}>{part}</b> : part))
-}
