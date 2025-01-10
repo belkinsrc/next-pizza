e93c46164aa34144778a01bb7fa05cf052f85f5a
Nikita Belkin <belkinsrc@gmail.com>
2025-01-10 18:56:00 +0300

    redo FilterCheckbox component

--- src/shared/ui/filter-checkbox/filter-checkbox.tsx
+++ src/shared/ui/filter-checkbox/filter-checkbox.tsx
@@ -1,34 +1,39 @@
+'use client'
+
 import { Checkbox } from '../../lib/checkbox'
 
 export interface FilterChecboxProps {
+  id: number
   text: string
   value: string
   endAdornment?: React.ReactNode
-  onCheckedChange?: (checked: boolean) => void
-  checked?: boolean
   name?: string
+  checked?: boolean
+  onCheckedChange?: (id: string) => void
 }
 
 const FilterCheckbox: React.FC<FilterChecboxProps> = ({
+  id,
   text,
   value,
   endAdornment,
-  onCheckedChange,
-  checked,
   name,
+  checked,
+  onCheckedChange,
 }) => {
   return (
     <div className="flex items-center space-x-2">
       <Checkbox
-        onCheckedChange={onCheckedChange}
-        checked={checked}
         value={value}
         className="rounded-[8px] w-6 h-6"
         id={`checkbox-${String(name)}-${String(value)}`}
+        checked={checked}
+        onCheckedChange={() => onCheckedChange?.(String(id))}
       />
       <label
         htmlFor={`checkbox-${String(name)}-${String(value)}`}
-        className="leading-none cursor-pointer flex-1">
+        className="leading-none cursor-pointer flex-1"
+      >
         {text}
       </label>
       {endAdornment}
