50fc2b9b638f4c6a64ebc2e6471aa66efbb317d1
Nikita Belkin <belkinsrc@gmail.com>
2025-01-10 18:52:40 +0300

    redo IngredientsFilter component

--- src/features/filter/ui/ingredients-filter/ingredients-filter.tsx
+++ src/features/filter/ui/ingredients-filter/ingredients-filter.tsx
@@ -1,43 +1,82 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useMemo } from 'react'
 import { Title } from '@components/shared/ui/title'
 import { FilterCheckbox } from '@components/shared/ui'
-import { Input } from '@/src/shared/lib/input'
-
-const ingredients = ['Сырный соус', 'Моцарелла', 'Чеснок', 'Солённые огурчики', 'Солённые огурчики', 'Томаты', 'Солённые огурчики', 'Солённые огурчики', 'Томаты']
+import { Input, Skeleton } from '@/src/shared/lib'
+import { useFilterIngredients } from '../../hooks'
 
 function IngredientsFilter() {
-  const [search, setSearch] = useState('')
-  const [filtetedIngredients, setFiltetedIngredients] = useState(ingredients)
-  const [isAllVisible, setIsAllVisible] = useState(false)
   const visibleItems = 6
-  const itemsCount = ingredients.length
+  const [search, setSearch] = useState('')
+  const [isAllVisible, setIsAllVisible] = useState(false)
+  const { ingredients, choosedIngredients, loading, toggle } =
+    useFilterIngredients()
+
+  const filteredIngredients = useMemo(() => {
+    if (search === '') return ingredients
+    return ingredients.filter((ingredient) => {
+      return ingredient.name.toLowerCase().includes(search.toLowerCase())
+    })
+  }, [ingredients, search])
 
   const handleAllVisibleItemsChange = () => {
     setIsAllVisible((prev) => !prev)
   }
 
   const handleSearchInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const searchedTerm = e.target.value
-    setSearch(searchedTerm)
-
-    const filtetedIngredients = ingredients.filter((ingredient) => {
-      return ingredient.toLowerCase().includes(searchedTerm.toLowerCase())
-    })
-    setFiltetedIngredients(filtetedIngredients)
+    setSearch(e.target.value)
   }
+
+  const displayedIngredients = isAllVisible
+    ? filteredIngredients
+    : ingredients.slice(0, visibleItems)
 
   return (
     <div className="pb-[42px]">
-      <Title text="Ингредиенты:" size="lg" className="mb-[15px] text-[16px] font-bold" />
-      {isAllVisible && <Input value={search} onChange={handleSearchInputChange} type="text" placeholder='Поиск...' className='mb-4 py-2 px-5' />}
+      <Title
+        text="Ингредиенты:"
+        size="lg"
+        className="mb-[15px] text-[16px] font-bold"
+      />
+      {isAllVisible && (
+        <Input
+          value={search}
+          onChange={handleSearchInputChange}
+          type="text"
+          placeholder="Поиск..."
+          className="mb-4 py-2 px-5"
+        />
+      )}
       <div className="flex flex-col gap-4 mb-[21px] h-[224px] overflow-y-auto">
-        {filtetedIngredients.slice(0, isAllVisible ? itemsCount : visibleItems).map((ingredient, i) => (
-          <FilterCheckbox key={i} text={ingredient} value={ingredient} name="ingredient" />
-        ))}
+        {loading
+          ? Array.from({ length: visibleItems }, (_, index) => {
+              return (
+                <Skeleton
+                  key={index}
+                  className="w-[100%] h-6 rounded-full bg-[#F1F1F1]"
+                />
+              )
+            })
+          : displayedIngredients?.map(({ id, name }) => (
+              <FilterCheckbox
+                id={id}
+                key={id}
+                text={name}
+                value={name}
+                name={name}
+                checked={choosedIngredients.has(String(id))}
+                onCheckedChange={(id: string) => toggle(id)}
+              />
+            ))}
       </div>
-      <button className='text-primary' onClick={handleAllVisibleItemsChange}>{isAllVisible ? '- Скрыть' : '+ Показать всё'}</button>
+      {loading ? (
+        <Skeleton className="w-28 h-6 rounded-full bg-[#F1F1F1]" />
+      ) : (
+        <button className="text-primary" onClick={handleAllVisibleItemsChange}>
+          {isAllVisible ? '- Скрыть' : '+ Показать всё'}
+        </button>
+      )}
     </div>
   )
 }
