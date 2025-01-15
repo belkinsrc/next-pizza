6aef3e57a37be129419b6e2bad2284ac68b89f5b
Nikita Belkin <belkinsrc@gmail.com>
2025-01-15 18:55:19 +0300

    remake component's logic

--- src/features/filter/ui/price-filter/price-filter.tsx
+++ src/features/filter/ui/price-filter/price-filter.tsx
@@ -1,81 +1,111 @@
 'use client'
 
+import { forwardRef, useState, useImperativeHandle } from 'react'
 import { Title } from '@components/shared/ui/title'
 import { Input } from '@components/shared/lib/input'
 import { RangeSlider } from '@components/shared/lib/slider'
-import { useFilterIngredients } from '../../hooks'
+import { cn } from '@/src/shared/utils'
 
-function PriceFilter() {
-  const { priceRange, setPriceRange } = useFilterIngredients()
+export type PriceFilterHandle = {
+  getState: () => { from: number; to: number }
+}
 
-  const handleSetMinValue = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value === '' ? 0 : Number(e.target.value)
+interface PriceFilterProps {
+  min: number
+  max: number
+  step: number
+  title: string
+  className?: string
+}
 
-    if (value > priceRange.max) {
-      return setPriceRange({
-        min: value,
-        max: value,
-      })
+const PriceFilter = forwardRef<PriceFilterHandle, PriceFilterProps>(
+  ({ className, title, min, max, step }, ref) => {
+    const [priceRange, setPriceRange] = useState({
+      from: min,
+      to: max,
+    })
+
+    useImperativeHandle(ref, () => {
+      return {
+        getState() {
+          return priceRange
+        },
+      }
+    }, [priceRange])
+
+    const handleSetMinValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const value = e.target.value === '' ? min : Number(e.target.value)
+
+      if (value > priceRange.to) {
+        return setPriceRange({
+          from: value,
+          to: value,
+        })
+      }
+
+      return setPriceRange((prev) => ({
+        ...prev,
+        from: value,
+      }))
     }
 
-    return setPriceRange((prev) => ({
-      ...prev,
-      min: value,
-    }))
-  }
+    const handleSetMaxValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const value = e.target.value === '' ? max : Number(e.target.value)
 
-  const handleSetMaxValue = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value === '' ? 1000 : Number(e.target.value)
+      if (value < priceRange.from) {
+        return setPriceRange({
+          from: value,
+          to: value,
+        })
+      }
 
-    if (value < priceRange.min) {
-      return setPriceRange({
-        min: value,
-        max: value,
-      })
+      return setPriceRange((prev) => ({
+        ...prev,
+        to: value,
+      }))
     }
 
-    return setPriceRange((prev) => ({
-      ...prev,
-      max: value,
-    }))
-  }
-
-  return (
-    <div className="pb-[30px] mb-[30px] border-b-[1px] border-[#F6F6F6]">
-      <Title
-        text="Цена от и до:"
-        size="lg"
-        className="mb-[15px] text-[16px] font-bold"
-      />
-      <div className="flex gap-[15px] mb-4">
-        <Input
-          type="number"
-          value={priceRange.min === 0 ? '' : priceRange.min}
-          onChange={handleSetMinValue}
-          min={0}
-          max={1000}
-          placeholder={String(0)}
-          className="rounded-[10px] py-[10px] px-[15px]"
+    return (
+      <div
+        className={cn(className, 'pb-[30px] border-b-[1px] border-[#F6F6F6]')}
+      >
+        <Title
+          text={title}
+          size="lg"
+          className="mb-[15px] text-[16px] font-bold"
         />
-        <Input
-          type="number"
-          value={priceRange.max === 1000 ? '' : priceRange.max}
-          onChange={handleSetMaxValue}
-          min={0}
-          max={1000}
-          placeholder={String(1000)}
-          className="rounded-[10px] py-[10px] px-[15px]"
+        <div className="flex gap-[15px] mb-4">
+          <Input
+            type="number"
+            value={priceRange.from === min ? '' : priceRange.from}
+            onChange={handleSetMinValue}
+            min={min}
+            max={max}
+            placeholder={String(min)}
+            className="rounded-[10px] py-[10px] px-[15px]"
+          />
+          <Input
+            type="number"
+            value={priceRange.to === max ? '' : priceRange.to}
+            onChange={handleSetMaxValue}
+            min={min}
+            max={max}
+            placeholder={String(max)}
+            className="rounded-[10px] py-[10px] px-[15px]"
+          />
+        </div>
+        <RangeSlider
+          value={[priceRange.from, priceRange.to]}
+          onValueChange={([from, to]) => setPriceRange({ from, to })}
+          min={min}
+          max={max}
+          step={step}
         />
       </div>
-      <RangeSlider
-        value={[priceRange.min, priceRange.max]}
-        onValueChange={([min, max]) => setPriceRange({ min, max })}
-        min={0}
-        max={1000}
-        step={10}
-      />
-    </div>
-  )
-}
+    )
+  }
+)
+
+PriceFilter.displayName = 'PriceFilter'
 
 export { PriceFilter }
