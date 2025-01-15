84ae99882a0dd2071e84d3d32fb1a0ff0f973b0c
Nikita Belkin <belkinsrc@gmail.com>
2025-01-15 18:54:54 +0300

    remake component's logic

--- src/features/filter/ui/addtitional-filter/additional-filter.tsx
+++ src/features/filter/ui/addtitional-filter/additional-filter.tsx
@@ -1,22 +1,44 @@
 'use client'
 
+import { forwardRef, useImperativeHandle, useMemo } from 'react'
+import { useSet } from 'react-use'
 import { FilterCheckbox } from '@components/shared/ui'
-import { useFilterIngredients } from '../../hooks'
 import { cn } from '@/src/shared/utils'
+
+export type AdditionalFilterHandle = {
+  getState: () => Set<string>
+}
 
 interface AdditionalFilterProps {
   className?: string
 }
 
-function AdditionalFilter({ className }: AdditionalFilterProps) {
-  const { additionalOptions, choosedAddtional, toggleChoosedAdditional } =
-    useFilterIngredients()
+const AdditionalFilter = forwardRef<
+  AdditionalFilterHandle,
+  AdditionalFilterProps
+>(({ className }, ref) => {
+  const [choosedAddtional, { toggle }] = useSet(new Set<string>([]))
+
+  const additionalOptions = useMemo(() => {
+    return [
+      { id: 'configurable', name: 'Можно собирать' },
+      { id: 'latest', name: 'Новинки' },
+    ]
+  }, [])
+
+  useImperativeHandle(ref, () => {
+    return {
+      getState() {
+        return choosedAddtional
+      },
+    }
+  }, [choosedAddtional])
 
   return (
     <div
       className={cn(
         className,
-        'flex flex-col gap-4 pb-[25px] mb-[25px] border-b-[1px] border-[#F6F6F6]'
+        'flex flex-col gap-4 pb-[25px] border-b-[1px] border-[#F6F6F6]'
       )}
     >
       {additionalOptions?.map(({ id, name }) => {
@@ -27,12 +49,14 @@ function AdditionalFilter({ className }: AdditionalFilterProps) {
             text={name}
             value={name}
             checked={choosedAddtional.has(id)}
-            onCheckedChange={() => toggleChoosedAdditional(id)}
+            onCheckedChange={() => toggle(id)}
           />
         )
       })}
     </div>
   )
-}
+})
+
+AdditionalFilter.displayName = 'AdditionalFilter'
 
 export { AdditionalFilter }
